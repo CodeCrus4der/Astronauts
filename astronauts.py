36fb3bd6b5b407d06a1be5b7ebc89c42af29ecfa
CodeCrus4der <mr.cheese.hu@gmail.com>
2023-03-03 18:50:23 +0100

    commit 3

--- astronauts.py
+++ astronauts.py
@@ -2,22 +2,22 @@ import pandas as pd
 import collections
 
 
-def get_grads():
+def get_major_list():
     df = pd.read_csv('astronauts.csv')
-    grad_majors = pd.concat([df['Graduate Major'], df['Undergraduate Major']])
-    grads = []
-    for grad in grad_majors:
-        if isinstance(grad, str):
-            grad = grad.split('&')
-            grad = [text.strip() for text in grad]
-            for txt in grad:
-                grads.append(txt)
-    return grads
+    major_series = pd.concat([df['Graduate Major'], df['Undergraduate Major']])
+    major_list = []
+    for major in major_series :
+        if isinstance(major, str):
+            major = major.split('&')
+            major = [text.strip() for text in major]
+            for txt in major:
+                major_list.append(txt)
+    return major_list
 
 
 def main():
     print('A program az "astronauts.csv" fájlból beolvasott asztronauták végzettségei közül kírja a 3 leggyakoribbat.')
-    grads = get_grads()
-    print('Leggyakoribb végzettségek')
-    for grad in collections.Counter(grads).most_common(3):
-        print(f'{grad[0]}, {round(grad[1] / len(grads) * 100, 1)}%')
+    major_list = get_major_list()
+    print('Leggyakoribb végzettségek:')
+    for major_tuple in collections.Counter(major_list).most_common(3):
+        print(f'{major_tuple[0]}, {round(major_tuple[1] / len(major_list) * 100, 1)}%')
