38386c0623657282c8f495af102ba46c0e55a7a5
CodeCrus4der <mr.cheese.hu@gmail.com>
2023-03-03 18:45:00 +0100

    commit 2

--- astronauts.py
+++ astronauts.py
@@ -1,21 +1,23 @@
 import pandas as pd
+import collections
 
-df = pd.read_csv('astronauts.csv')
-grad_majors = pd.concat([df['Graduate Major'], df['Undergraduate Major']])
 
-grads = []
-grads_count = []
-for grad in grad_majors:
-    if isinstance(grad, str):
-        grad = grad.split('&')
-        grad = [text.strip() for text in grad]
-        for txt in grad:
-            if txt not in grads:
+def get_grads():
+    df = pd.read_csv('astronauts.csv')
+    grad_majors = pd.concat([df['Graduate Major'], df['Undergraduate Major']])
+    grads = []
+    for grad in grad_majors:
+        if isinstance(grad, str):
+            grad = grad.split('&')
+            grad = [text.strip() for text in grad]
+            for txt in grad:
                 grads.append(txt)
-                grads_count.append(1)
-            else:
-                grads_count[grads.index(txt)] += 1
+    return grads
 
-print(grads)
-print(grads_count)
 
+def main():
+    print('A program az "astronauts.csv" fájlból beolvasott asztronauták végzettségei közül kírja a 3 leggyakoribbat.')
+    grads = get_grads()
+    print('Leggyakoribb végzettségek')
+    for grad in collections.Counter(grads).most_common(3):
+        print(f'{grad[0]}, {round(grad[1] / len(grads) * 100, 1)}%')
